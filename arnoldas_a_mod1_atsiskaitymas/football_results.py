54e3c8fc0671b2498f0b4bc483f804b5a2f3b350
Arnold <ambrasas.arnoldas@gmail.com>
2024-11-21 18:10:12 +0200

    Done with football results website, added requirements. Later on will merge two different websites under one class

---
+++ arnoldas_a_mod1_atsiskaitymas/football_results.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+path = 'C:\\Tools\\chromedriver\\chromedriver.exe'
+
+service = Service(path)
+driver = webdriver.Chrome(service=service)
+
+
+driver.get(website)
+
+
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//label[@analytics-event="All matches"]'))
+).click()
+
+
+date = []
+home_team = []
+score = []
+away_team = []
+
+
+matches = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.TAG_NAME, "tr"))
+)
+
+
+for match in matches:
+    try:
+
+        tds = match.find_elements(By.TAG_NAME, 'td')
+        if len(tds) >= 4:
+            date.append(tds[0].text)
+            home_team.append(tds[1].text)
+            score.append(tds[2].text)
+            away_team.append(tds[3].text)
+    except Exception as e:
+        print(f"Skipping a row due to an error: {e}")
+
+
+for i in range(len(date)):
+    print(f"Date: {date[i]}, Home Team: {home_team[i]}, Score: {score[i]}, Away Team: {away_team[i]}")
+
+
+driver.quit()
